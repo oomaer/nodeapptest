dadda33494b07b31b2c1bcf223bc76d823898331
umair <cyumair@hotmail.com>
2023-01-08 13:33:15 +0500

    changes

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
 
 const webdriver = require('selenium-webdriver');
 const assert = require('assert');
-const { Options } = require('selenium-webdriver/chrome');
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
 
-options = new Options()
+
+options = new chrome.Options()
 options.headless();
 
 const driver = new webdriver.Builder().setChromeOptions(options).forBrowser('chrome').build();
@@ -15,8 +17,8 @@ describe('Show Error Message on Empty Input', () => {
             driver.findElement(webdriver.By.name('email')).sendKeys('');
             driver.findElement(webdriver.By.css('button')).click();
             
-            //wait for 5 seconds if error message is not shown
-            await driver.wait(webdriver.until.elementTextIs(driver.findElement(webdriver.By.id("loginMessage")), "Invalid email or password"), 5000);
+            //wait for 7 seconds if error message is not shown
+            await driver.wait(webdriver.until.elementTextIs(driver.findElement(webdriver.By.id("loginMessage")), "Invalid email or password"), 7000);
             let text = await driver.findElement(webdriver.By.id('loginMessage')).getText();
             assert.equal(text, "Invalid email or password");
             driver.quit();
